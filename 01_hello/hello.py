a2568bdfc382562096d497abe9e433711f85a515
AnguaTheCityWatch <folate.opus_0c@icloud.com>
2023-02-18 17:02:24 +0000

    Completed 01_hello practice

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python3
-#Say Hello :)
+"""
+Autor: Ken Youncen-Clark
+Purpose: Say Hello, while learning Python basics.
+"""
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('-n', '--name', metavar='name', default='World', help='Name to greet')
-args = parser.parse_args()
-name = args.name
-print('Hello, ' + name + '!')
+
+def get_args():
+    """
+    Get the command line arguments
+    """
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n',
+                        '--name',
+                        metavar='name',
+                        default='World',
+                        help='Name to greet')
+    return parser.parse_args()
+
+
+def main():
+    """
+    This is where the job is done
+    """
+    args = get_args()
+    name = args.name
+    print('Hello, ' + name + '!')
+
+
+if __name__ == '__main__':
+    main()
